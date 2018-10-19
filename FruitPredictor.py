55d8672775e78d36780b64f74c536714a8f21176
Dryan López Fernández <dryanlf.dlf@gmail.com>
2018-10-19 01:09:54 -0500

    Algo extra
    Automatización

--- FruitPredictor.py
+++ FruitPredictor.py
@@ -2,8 +2,14 @@ from sklearn import tree
 #1 naranja, 2 manzana
 #1 rugosa, 2 lisa
 #         [[Masa(g),Textura]]
+
 features= [[150,1],[170,1],[140,2],[130,2]]
+#Características
 labels= [1,1,2,2]
+#Resultados
+
 clf= tree.DecisionTreeClassifier()
+
 clf=clf.fit(features,labels)
+
 print clf.predict([[150,1]])
